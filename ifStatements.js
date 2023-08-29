215b641b10a291543ea18b4753ef62ec695c0cae
Dana Zack <danaezack@gmail.com>
2023-08-29 17:12:30 -0600

    Complete challenge 2

--- ifStatements.js
+++ ifStatements.js
@@ -8,15 +8,13 @@
 // Write a conditional statement that prints only one or the other.
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
-var numQuarters = 0;
+var numQuarters = 2;
 
 if (numQuarters >= 2){
     console.log("I have enough money for a gumball");
 } else {
     console.log("I don't have enough money for a gumball");
 }
-
-
 
 
 // CHALLENGE 2
@@ -31,6 +29,15 @@ if (numQuarters >= 2){
 
 // Experiment with manipulating the value held within both variables to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 3;
 var hasSauce = true;
 
+if (cupsOfFlour >=2 && hasSauce === true) {
+    console.log("I can make pizza");
+} else if (cupsOfFlour <2 && hasSauce === true){
+    console.log("I cannot make pizza");
+} else if (cupsOfFlour <2 && hasSauce === false){
+    console.log("I cannot make pizza")
+} else {
+    console.log("I cannot make pizza")
+}
